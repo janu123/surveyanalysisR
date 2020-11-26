6b93e24757cf913cd7992c794693b303d45438a3
janu123 <janani6490.selva@gmail.com>
2020-11-26 16:02:58 +0530

    Initial commit

--- R/data.R
+++ R/data.R
@@ -4,7 +4,7 @@
 #'
 #' @format A data frame with 10704 rows and 9 variables:
 #' \describe{
-#'   \item{response_id}{}
+#'   \item{response_id}{response survey id}
 #'   \item{task}{}
 #'   \item{duration}{}
 #'   \item{offer}{}
